178dfc6b57ecf1c3bd856b9ee93fa201acbd53de
gabriel <gabrielrsp18@gmail.com>
2022-04-24 16:52:19 -0300

    load data by ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
+// import { GetServerSideProps } from 'next' //LIKE API CALL SSR
+import { GetStaticProps } from 'next' //STATIC PAGE CALL SSG
 import Head from 'next/head' // this is a react component
 import { SubscribeButton } from '../components/SubscribeButton'
-
+import { stripe } from '../services/stripe'
 import styles from './home.module.scss'
 
-export default function Home () {
+/*
+ 3 ways to populate a page with data
+
+  - Client Side -> when no need of indexation, or data that doesn't need to be already loaded, or data loaded by an action from user like comments of blog
+  - Server Side -> when needed specific data from an user also indexation - Careful, because if there's a lot of processing here, it will took longer to show the page
+  - Static Side -> information that are same for everyone and need indexation (home of blog, product page, and indexation needed)
+
+*/
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home ({ product }: HomeProps) {
   return (
     <>
       {/* the title can be passed dynamically on each page */}
@@ -18,13 +36,69 @@ export default function Home () {
           <h1>News about the <span>React</span> world,</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton/>
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
 
     </>
   )
+}
+
+// /*
+//  calling api via SSR
+//  must be inside a PAGE and then passed to a component 
+// */
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const price = await stripe.prices.retrieve('price_1Ks8SaFt5so6u4LnhofTgITM', {
+//     expand: ['product']
+//   })
+
+//   const product = {
+//     priceId: price.id,
+//     amount: new Intl.NumberFormat('en-US', {
+//       style: 'currency',
+//       currency: 'USD',
+//     }).format(price.unit_amount / 100)
+
+//   }
+
+//   return {
+//     props: {
+//       product,
+//     }
+//   }
+// }
+
+// getServerSideProps is executed on the SERVER SIDE!
+
+/*
+getting static page SSG
+ must be inside a PAGE and then passed to a component 
+ NOTE: THIS STATIC CONTENT WILL BE THE SAME AND RETRIEVED FOR ALL USERS
+ SO THIS CAN BE USED ONLY IN PAGES WHERE THE CONTENT WILL BE THE SAME FOR ANYONE
+*/
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Ks8SaFt5so6u4LnhofTgITM', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100)
+
+  }
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24 // 24hours defined for how long in sec the content of page will not revalidate
+
+  }
 }
---
+++ src/services/stripe.ts
@@ -0,0 +1,14 @@
+//Stripe is a SDK (software development kit)
+import Stripe from 'stripe'
+import { version } from '../../package.json'
+
+export const stripe = new Stripe(
+  process.env.STRIPE_API_KEY,
+  {
+    apiVersion: '2020-08-27',
+    appInfo: {
+      name: 'Ignews',
+      version
+    },
+  }
+)
